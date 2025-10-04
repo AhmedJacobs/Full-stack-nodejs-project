5e5e625f0c48eb1af5d92abb7e4fb86027b727a6
Ahmed yaacoub <ahmedyaacoub2003@gmail.com>
2025-10-04 17:09:56 +0100

    challange number 1

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-// index.js
-import http from 'http';
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const PORT = 8000
 
+/*
+Challenge 1:
+
+1. Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
+*/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from ES Module Server!</h1>');
-});
 
-const port = 8000;
-server.listen(port, () => { console.log(`Server running at http://localhost:${port}/`);});
+/*
+Challenge 3:
+
+1. Import and call serveStatic and pass it the directory of this current module.
+*/
+serveStatic(__dirname);
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><h1>The server is working</h1></html>')
+})
+
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
---
+++ utils/serveStatic.js
@@ -0,0 +1,18 @@
+/*
+Challenge 2:
+
+1. Create and export a function called 'serveStatic'. 
+   It should take in the base directory as a parameter.
+
+2. Build a path to index.html in the 'public' folder and save it to a const 'filePath'. 
+   (Which node module will you need to import to do this? Which method joins the path together?)
+
+3. Log 'filePath' to the console.
+*/
+import path from 'path';
+export const serveStatic = (baseDir) => {
+  console.log(`The base directory for static files is: ${baseDir}`);
+  // The logic for serving files would go here.
+  const filePath = path.join(baseDir , 'public' , 'index.html');
+  console.log(filePath);
+};
