4a8f0b2227a3ca908527cb664dd625c46d49e3f5
Ahmed yaacoub <ahmedyaacoub2003@gmail.com>
2025-10-05 16:43:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,22 +3,12 @@ import { serveStatic } from './utils/serveStatic.js'
 
 const PORT = 8000
 
-/*
-Challenge 1:
 
-1. Get the name of the directory holding this server.js file and store it to a const ‘__dirname’.
-*/
 
 const __dirname = import.meta.dirname
 
 
 const server = http.createServer((req, res) => {
-
-/*
-Challenge 3:
-
-1. Import and call serveStatic and pass it the directory of this current module.
-*/
     serveStatic(__dirname)
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
