31ff555232bc3cc77f72c30d8e8e28d72d5e1754
Ahmed yaacoub <ahmedyaacoub2003@gmail.com>
2025-10-07 18:47:13 +0100

    process post data
    Using the Built-in http Module

--- handlers/routeHandlers.js
+++ handlers/routeHandlers.js
@@ -2,6 +2,21 @@ import {getData} from '../utils/getData.js'
 import { sendResponse } from '../utils/sendResponse.js';
 
 
+async function parseJSONBody(req) {
+    return new Promise((resolve, reject) => {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            try {
+                resolve(JSON.parse(body));
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
+}
 
 export async function handleGet(req , res ){
     const data = await getData();
@@ -9,9 +24,14 @@ export async function handleGet(req , res ){
     sendResponse(res , 200 , 'application/json' , stringifiedData); 
 }
 
+export async function handlePost(req, res) {
+    // 1. Log the required message to the console
+    console.log('POST request received');
 
+    // 2. Send a simple response to close the connection
+    res.end('POST request handled.');
+}
 // handlePost
-
 // parseJSONBody() will collect and parse the incoming JSON
 // santizeData() 
 // addNewSighting() will do the donkey work of adding the data to our dataset
--- server.js
+++ server.js
@@ -1,29 +1,22 @@
-import http from 'node:http'
-import { serveStatic } from './utils/serveStatic.js'
-import { handleGet } from './handlers/routeHandlers.js'
-const PORT = 2500
+import http from 'node:http';
+import { serveStatic } from './utils/serveStatic.js';
+// 1. Import handlePost along with handleGet
+import { handleGet, handlePost } from './handlers/routeHandlers.js';
 
-const __dirname = import.meta.dirname
+const PORT = 2500;
+const __dirname = import.meta.dirname;
 
 const server = http.createServer(async (req, res) => {
-
-if(req.url.startsWith('/api')){
-    if (req.method === 'GET'){
-        return await handleGet(req, res); 
+    if (req.url.startsWith('/api')) {
+        if (req.method === 'GET') {
+            return await handleGet(req, res);
+        } else if (req.method === 'POST') {
+            // 2. Call handlePost, not handleGet
+            return await handlePost(req, res);
+        }
+    } else {
+        return await serveStatic(req, res, __dirname);
     }
-    else if (req.method === 'POST'){
-        return await handleGet(req , res) ; 
-    }
+});
 
-    /*
-Challenge: 
-   1. Add a route for a POST request to '/api'.
-   2. When a request comes in, pass the req and res to handlePost().
-*/
-}else {
-    return await serveStatic(req , res , __dirname); 
-}
-}) ; 
-
-server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
- 
+server.listen(PORT, () => console.log(`Connected on port: ${PORT}`));
